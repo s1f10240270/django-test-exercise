df0a5e4697c70967fdad2e3c11a89ff56003165a
1F10240270 <s1f102402709@iniad.org>
2025-07-03 11:56:14 +0900

    空行追加

--- todo/tests.py
+++ todo/tests.py
@@ -2,7 +2,8 @@ from django.test import TestCase, Client
 from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
-# Create your tests here.
+
+
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
